de6854e29c6840502baf524846793f0c40559ac5
Jiří Zdvomka <jiri.zdvomka@ackee.cz>
2021-04-18 22:00:14 +0200

    ✨  Add progress bar

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
-from LectureVideoIndexer import LectureVideoIndexer
+from tqdm import tqdm
 
+from LectureVideoIndexer import LectureVideoIndexer
 from Config import Config
 from Stage import Stage
 
 def handleProgress(stage: Stage, progress: float):
-    print("Progress: ", progress)
+    bar.update(progress - bar.n)
 
 if __name__ == '__main__':
     indexer = LectureVideoIndexer(config={ 'frameStep' : 2 }, progressCallback=handleProgress)
+
+    bar = tqdm(total = 100)
     indexer.index('video/trimmed.mp4')
+    bar.close()
