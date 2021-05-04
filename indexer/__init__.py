dc6a8c1d1016bace239ec3b312b5e0f853b1eb29
Jiří Zdvomka <jiri.zdvomka@ackee.cz>
2021-05-04 15:02:46 +0200

    🔖  Release version 0.1.1

--- indexer/__init__.py
+++ indexer/__init__.py
@@ -2,5 +2,5 @@ from .video_indexer import LectureVideoIndexer
 from .Types import Config, Stage
 from .video_converter import VideoConverter, CropRegion
 
-__name__ = 'strsimpy'
-__version__ = "0.1.0"
+__name__ = 'lecture_video_indexer'
+__version__ = "0.1.1"
