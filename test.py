988966cec01144ef037dd111e5f921f8482726df
Jiří Zdvomka <jiri.zdvomka@ackee.cz>
2021-12-06 15:35:33 +0100

    ✅ Parametrise test script

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 # Compare an output to the reference test data
 
+import argparse
 import json
 import os
 
 from tqdm import tqdm
 from indexer import LectureVideoIndexer, Stage, CropRegion
+from utils.Intersector import Intersector
 
 current_stage = None
 test_videos_path = os.environ.get('TEST_VIDEO_PATH')
+
 
 def list_diff(li1, li2):
     return (list(list(set(li1) - set(li2)) + list(set(li2) - set(li1))))
@@ -19,42 +22,68 @@ def handle_progress(bar, stage: Stage, progress: float):
     bar.update(progress - bar.n)
 
 
-def compare_index(ref_video, treshold):
+def compare_index(ref_video, config):
     print(f"Creating index for {ref_video['name']}")
     bar = tqdm(total=100)
     current_stage = None
 
-    config = {'frame_step': 2, 'hash_size': 16, 'image_similarity_treshold': treshold}
-    crop_region = CropRegion(0, 80, 890, 1700)
+    # crop_region = CropRegion(0, 80, 890, 1700)
 
     indexer = LectureVideoIndexer(
         config=config, progress_callback=lambda stage, progress: handle_progress(bar, stage, progress))
-    index = indexer.index(video_path=os.path.join(test_videos_path, ref_video['name']), crop_region=crop_region)
+    index = indexer.index(video_path=os.path.join(test_videos_path, ref_video['name']))
     seconds = [entry['second'] for entry in index]
     print(seconds)
     bar.close()
 
-    intersection_cnt = len(list(set(seconds).intersection(set(ref_video['index']))))
+    # Intersection with a custom equivalence metric considering error in seconds
+    intersector = Intersector(lambda x, y: abs(x - y) <= config['frame_step'])
+    intersection_cnt = len(list(intersector.intersect(set(seconds), set(ref_video['index']))))
     precision = intersection_cnt / len(ref_video['index'])
     print(precision)
     return precision
 
 
 if __name__ == '__main__':
-    tresholds = [0.9]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--param', dest="param", help="Name of the observed config param", required=True)
+    parser.add_argument('--values', dest="values", help="Comma separated values", required=True)
 
-    with open('test_data/reference.json') as json_data:
+    args = parser.parse_args()
+    observed_param = args.param
+    values = [
+        int(x) if observed_param == 'frame_step' or observed_param == 'hash_size' else float(x)
+        for x in args.values.split(',')
+    ]
+    results = []
+
+    config = {
+        'frame_step': 2,
+        'hash_size': 16,
+        'image_similarity_threshold': 0.9,
+        'text_similarity_treshold': 0.85
+    }
+
+    with open('test_data/reference.test.json') as json_data:
         test_data = json.load(json_data)
 
-        for treshold in tresholds:
-            print("Running treshold ", treshold)
+        for value in values:
+            print(f"{observed_param} value: {value}")
             precisions = []
 
+            config[observed_param] = value
+
             for video in test_data:
-                index = compare_index(video, treshold)
+                index = compare_index(video, config)
                 precisions.append(index)
 
-            avg_precision = sum(precisions) / len(test_data)
+            results.append({
+                'value': value,
+                'avg_precision': round(sum(precisions) / len(test_data), 3),
+                'max_precision': max(precisions),
+                'min_precision': min(precisions)
+            })
 
-            with open('output/measurements.txt', 'a') as output_file:
-                output_file.write(f"Treshold {treshold}: {round(avg_precision, 3)}\n")
+    with open(f'output/{observed_param}_results.json', 'w') as output_file:
+        json = json.dumps(results, indent=4)
+        output_file.write(json)
